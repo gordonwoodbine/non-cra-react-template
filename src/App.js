9cd9e10ac64f545766f3b9294fb52c4f823869d0
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2023-01-10 21:02:46 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <p>I am your new app.</p>
+      <p>Cherish me.</p>
+      <p>Then dismember me.</p>
+    </div>
+  );
+};
+
+export default App;
